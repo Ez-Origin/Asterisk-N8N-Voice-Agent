edf6bffa9679393f24175529517c8a1f525d9d95
Malaka Wickremasinghe <malaka.lion@gmail.com>
2025-10-30 13:16:39 +0530

    updated engin.py play recoded audio

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import asyncio
 import os
-import sys
-
-# Add the 'src' directory to the Python path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
-
-from engine import main
+from src.engine import main
 
 if __name__ == "__main__":
     try:
